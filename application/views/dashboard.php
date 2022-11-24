32bce02551d4bfccb5388bf66651f3d8880b4cd0
Sayed Saddam Hussain <48250591+sayedsaddam@users.noreply.github.com>
2022-11-25 00:51:23 +0500

    updated dashboard.php, changed the card labels on it.

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -12,15 +12,15 @@
 	<!-- Content Row -->
 	<div class="row">
 
-			<!-- Earnings (Monthly) Card Example -->
+			<!-- Clients (Monthly) Card Example -->
 			<div class="col-xl-3 col-md-6 mb-4">
 				<div class="card border-left-primary shadow h-100 py-2">
 					<div class="card-body">
 							<div class="row no-gutters align-items-center">
 								<div class="col mr-2">
 									<div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-											Earnings (Monthly)</div>
-									<div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
+											Clients (Monthly)</div>
+									<div class="h5 mb-0 font-weight-bold text-gray-800">400</div>
 								</div>
 								<div class="col-auto">
 									<i class="fas fa-calendar fa-2x text-gray-300"></i>
@@ -37,8 +37,8 @@
 							<div class="row no-gutters align-items-center">
 								<div class="col mr-2">
 									<div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-											Earnings (Annual)</div>
-									<div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
+											Earnings (Monthly)</div>
+									<div class="h5 mb-0 font-weight-bold text-gray-800">PKR 200,000</div>
 								</div>
 								<div class="col-auto">
 									<i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
@@ -84,8 +84,8 @@
 							<div class="row no-gutters align-items-center">
 								<div class="col mr-2">
 									<div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-											Pending Requests</div>
-									<div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
+											Pending Tasks</div>
+									<div class="h5 mb-0 font-weight-bold text-gray-800" title="Glasses to be made...">18</div>
 								</div>
 								<div class="col-auto">
 									<i class="fas fa-comments fa-2x text-gray-300"></i>
